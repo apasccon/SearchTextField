0f06756c0bdfac01ed16c1bc7365af00f2a475e0
Andrea Stevanato <runner_corsa@msn.com>
2018-06-27 09:07:18 +0200

    Fix Y offset when the table view direction is up

--- SearchTextField/Classes/SearchTextField.swift
+++ SearchTextField/Classes/SearchTextField.swift
@@ -310,9 +310,9 @@ open class SearchTextField: UITextField {
                 shadowView!.frame = shadowFrame
             } else {
                 let tableHeight = min((tableView.contentSize.height), (UIScreen.main.bounds.size.height - frame.origin.y - theme.cellHeight))
-                UIView.animate(withDuration: 0.2, animations: { [weak self] in
-                    self?.tableView?.frame = CGRect(x: frame.origin.x + 2, y: (frame.origin.y - tableHeight), width: frame.size.width - 4, height: tableHeight)
-                    self?.shadowView?.frame = CGRect(x: frame.origin.x + 3, y: (frame.origin.y + 3), width: frame.size.width - 6, height: 1)
+                UIView.animate(withDuration: 0.2, animations: { [unowned self] in
+                    self.tableView?.frame = CGRect(x: frame.origin.x + 2, y: (frame.origin.y - tableHeight - self.tableYOffset), width: frame.size.width - 4, height: tableHeight)
+                    self.shadowView?.frame = CGRect(x: frame.origin.x + 3, y: (frame.origin.y + 3 - self.tableYOffset), width: frame.size.width - 6, height: 1)
                 })
             }
             
