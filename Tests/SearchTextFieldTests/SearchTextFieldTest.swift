91ee25c8369ba2d9ca054d8003d5d50944b82af9
Brent Mifsud <brentmifsud@icloud.com>
2020-05-15 16:24:27 -0400

    Add swiftlint, swiftformat
    - clean up most swiftlint errors and warnings.

--- Tests/SearchTextFieldTests/SearchTextFieldTest.swift
+++ Tests/SearchTextFieldTests/SearchTextFieldTest.swift
@@ -1,25 +1,25 @@
 //
 //  SearchTextFieldTest.swift
-//  
+//
 //
 //  Created by Brent Mifsud on 2020-05-15.
 //
 
 #if canImport(UIKit)
-import XCTest
-@testable import SearchTextField
+	@testable import SearchTextField
+	import XCTest
 
-class SearchTextFieldTest: XCTestCase {
-	override func setUp() {
-		// add setup code
-	}
+	class SearchTextFieldTest: XCTestCase {
+		override func setUp() {
+			// add setup code
+		}
 
-	override func tearDown() {
-		// add tear down code
-	}
+		override func tearDown() {
+			// add tear down code
+		}
 
-	func testSearchTextField() {
-		// add test code
+		func testSearchTextField() {
+			// add test code
+		}
 	}
-}
 #endif
