f620e9607ce508d96c80b7f43c6bc812040b1dd4
Alexander Kormanovsky <kormanowskij@yandex.ru>
2021-06-23 10:37:07 +0300

    add testing setup

--- Example - search range/SearchTextFieldWithRange/SearchTextFieldWithRange/ViewController.swift
+++ Example - search range/SearchTextFieldWithRange/SearchTextFieldWithRange/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SearchTextField
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var searchTextField: SearchTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
 
+    func setup() {
+        
+        var testArr = [SearchTextFieldItem]()
+
+        for i in 0..<10 {
+            testArr.append(SearchTextFieldItem(title: "title \(i)", subtitle: "subtitle \(i)"))
+        }
+
+        searchTextField.filterItems(testArr)
+
+        searchTextField.maxNumberOfResults = 10
+        searchTextField.minCharactersNumberToStartFiltering = 2
+        searchTextField.typingStoppedDelay = 0.4
+
+        searchTextField.theme.font = UIFont.systemFont(ofSize: 17)
+        searchTextField.theme.bgColor = .white
+
+        searchTextField.itemSelectionHandler = { filteredResults, itemPosition in
+            let item = filteredResults[itemPosition]
+            // ...
+        }
+
+        searchTextField.userStoppedTypingHandler = {
+            if let text = self.searchTextField.text, text.count >= self.searchTextField.minCharactersNumberToStartFiltering {
+                self.searchTextField.showLoadingIndicator()
+
+                // ...
+
+                self.searchTextField.stopLoadingIndicator()
+            }
+        }
+        
+    }
 
 }
 
